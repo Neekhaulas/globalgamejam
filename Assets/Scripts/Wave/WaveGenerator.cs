ea24d44425ff0232a14a16bf74a6a7dc40cd8803
exidhor <jipay.detiege@gmail.com>
2017-01-20 20:26:52 +0100

    Add wavepoint

---
+++ Assets/Scripts/Wave/WaveGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour
+{
+    public float Amplitude;
+    public float AngularFrequency;
+    public float Phase;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Wave/WavePoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class WavePoint
+{
+    public float Height;
+
+    public WavePoint()
+        : this(0)
+    { }
+
+    public WavePoint(float height)
+    {
+        Height = height;
+    }
+}
+

beec9212ff2c39e4773463ab578bd9b294ea9a56
coquicox <nicolasroy96@gmail.com>
2017-01-21 20:55:56 +0100

    CA MARCHE

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -6,6 +6,19 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+    public float MoveSpeed;
 
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Vector2 velocity = _rigidbody2D.velocity;
+        velocity.x = MoveSpeed*move;
+        _rigidbody2D.velocity = velocity;
+    }
 }
-

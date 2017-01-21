9cb96d7aa6ebdec89fb486dd50fdcd46622dfd02
coquicox <nicolasroy96@gmail.com>
2017-01-21 22:22:52 +0100

    Coule

--- Assets/Scripts/Player/Ship.cs
+++ Assets/Scripts/Player/Ship.cs
@@ -9,6 +9,7 @@ public class Ship : MonoBehaviour
 {
     public float Height;
     public GameObject Water;
+    public float SinkAngle = 90f;
     private Rigidbody2D _rigidbody2D;
     private float _startPosition;
 
@@ -31,6 +32,13 @@ public class Ship : MonoBehaviour
             positionToMove.x = _startPosition;
             _rigidbody2D.MovePosition(positionToMove);
             transform.position = positionToMove;
+        }
+        Debug.Log(transform.eulerAngles.z);
+        if (transform.rotation.z > 0.707 || transform.rotation.z < -0.707)
+        {
+            Debug.Log("Sink");
+            _rigidbody2D.constraints = RigidbodyConstraints2D.None;
+            enabled = false;
         }
     }
 }

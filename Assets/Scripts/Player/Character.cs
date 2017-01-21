78399f5b38d3dc3b3e36f2b2970031be018d76f6
exidhor <jipay.detiege@gmail.com>
2017-01-21 19:27:28 +0100

    push smth

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
-namespace Assets.Scripts.Player
+public class Character : MonoBehaviour
 {
-    class Character
-    {
-    }
+
 }
+

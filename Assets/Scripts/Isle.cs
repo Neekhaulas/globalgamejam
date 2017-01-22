bfb48d6bb778a362dadfcf89f4f0abadba0d8a6a
exidhor <jipay.detiege@gmail.com>
2017-01-22 16:39:28 +0100

    add the wonderfull isle

---
+++ Assets/Scripts/Isle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class Isle : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        Vector2 position = transform.position;
+        position.x -= speed * Time.deltaTime;
+        transform.position = position;
+    }
+}
+
